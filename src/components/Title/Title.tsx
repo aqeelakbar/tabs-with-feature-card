dab152b8eafbd5b124c7434dfd289927f6c71467
Aqeel Akbar <783538+aqeelakbar@users.noreply.github.com>
2026-09-23 08:52:12 +0200

    Replace placeholder heading in live demo

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -3,6 +3,6 @@ import styles from './Title.module.css';
 
 export default function Title() {
   return (
-    <h1 className={styles.heading1}>Short and snappy title</h1>
+    <h1 className={styles.heading1}>Make more of your money</h1>
   );
 }
